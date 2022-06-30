61afb3968d0af3ce389833d234502697fb963bf8
RayDev1988 <devsops2022@gmail.com>
2022-06-30 16:48:51 -0700

    Delete button

--- nextjs/pages/index.js
+++ nextjs/pages/index.js
@@ -6,7 +6,6 @@ function HomePage(props) {
   return (
     <section className="home">
       <Header />
-      <Button color="primary">Hello World</Button>
       <img src={"/HomePage.jpg"} alt="Home Page" className="absolute top-6" />
       <p>
         photo can originally be found{" "}
