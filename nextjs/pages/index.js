52ebb9f98c46a73090b6567c3df91f1f00d75d9c
RayDev1988 <devsops2022@gmail.com>
2022-06-30 14:15:22 -0700

    Test project

--- nextjs/pages/index.js
+++ nextjs/pages/index.js
@@ -1,15 +1,13 @@
 import React from "react";
 import Header from "./components/Header";
+import { Button } from '@material-ui/core';
 
 function HomePage(props) {
   return (
     <section className="home">
       <Header />
-      <main className="dark">
-        <h3 className="dark:hidden">Light mode</h3>
-        <h3 className="hidden dark:block">Dark mode</h3>
-      </main>
-      <img src={"/HomePage.jpg"} alt="Home Page" />
+      <Button color="primary">Hello World</Button>
+      <img src={"/HomePage.jpg"} alt="Home Page" className="absolute top-6" />
       <p>
         photo can originally be found{" "}
         <a href="https://starwars.fandom.com/wiki/The_Mandalorian">here</a>
